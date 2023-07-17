f89581310d9a9fb7718cccb1664b2b7f70f8ee53
John Nunley <jtnunley01@gmail.com>
2023-07-16 22:32:48 -0700

    Add smol-rs logo (#41)

--- src/lib.rs
+++ src/lib.rs
@@ -77,6 +77,12 @@
 
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 #![forbid(unsafe_code)]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/smol-rs/smol/master/assets/images/logo_fullsize_transparent.png"
+)]
 
 use std::any::Any;
 use std::collections::VecDeque;
